ed7c2755642969681bc9d75aaa83f55f83efb26a
seb@kickseed <seb@kickseed>
2017-09-13 16:20:23 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate xml;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
+
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size).map(|_| INDENT)
+             .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
+}
+
+fn main() {
+    let file = File::open("description.xml").unwrap();
+    let file = BufReader::new(file);
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{}+{}", indent(depth), name.local_name);
+                depth += 1;
+            }
+
+            Ok(XmlEvent::Characters(msg)) => {
+//                println!("{}", msg);
+                println!("{}+{}", indent(depth), msg);
+
+            }
+
+
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{}-{}", indent(depth), name.local_name);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
+}
